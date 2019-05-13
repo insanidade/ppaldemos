4762e6fcb60b9f5f897ed13709490ef51cd5b9d0
Otávio Augusto <otavio.augusto@gmail.com>
2019-05-13 00:34:16 -0300

    testing for us currency

--- client/src/properties.js
+++ client/src/properties.js
@@ -1,5 +1,7 @@
 export const properties = {
     content: "content test",
-    sandbox_client_id: "AX9h2N5Rwp8xXXoC1SFbw9eehS6FTfpJjI0WaSBLB_minEkK0FcFM8hDcvhH5wZoyBSdd5RlfhWsdWbK",
-    sandbox_secret: "EMv6FfzFSJqNFJt5r-0CsszYzv7dFAHcQpFAJ3tUFHgfe2B9Y0CW1ZMaoLXehkXEJd9BB_dSXYae_wj5"
+    sandbox_client_id: "ASpwwK3e6Xq319fcTEY4asiXBYzRZQK3kJLVZH5mQYf_7ZJw7cKzIScarLFGWwqcObuTKKYMPw6RLADw",
+    sandbox_secret: "EJWh8j2_IvgH-4CWwCnqrWOgvj_epwM0YCNrCRKfevUS9GIH04NEiK27H7hna3JofiRZ7hUj789aDX6j"
+    //sandbox_client_id: "AX9h2N5Rwp8xXXoC1SFbw9eehS6FTfpJjI0WaSBLB_minEkK0FcFM8hDcvhH5wZoyBSdd5RlfhWsdWbK",
+    //sandbox_secret: "EMv6FfzFSJqNFJt5r-0CsszYzv7dFAHcQpFAJ3tUFHgfe2B9Y0CW1ZMaoLXehkXEJd9BB_dSXYae_wj5"
 };
